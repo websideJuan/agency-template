cdca69c9c09b1972801a91df1786e01d08238c52
juanHerrera <portador1051@gmail.com>
2025-07-17 22:37:22 -0400

    Update UI

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 const btnToggleMenu = document.querySelector(".navbar-toggle");
 const navbar = document.querySelector("#navbar");
 
+window.addEventListener("DOMContentLoaded", () => main());
 
-document.addEventListener("click", (e) => {
-  const contain = e.target.classList.contains("navbar-toggle");
-  
-  if (contain) {
+
+function main() {
+  document.addEventListener("click", (e) => handlerToggleNavbar(e.target));
+
+  function handlerToggleNavbar(target) {
+    if (target.classList.contains("navbar-container")) {
+      navbar.classList.remove("toggle-active");
+      btnToggleMenu.setAttribute("aria-expanded", "false");
+      return;
+    }
+
+    if (!target.classList.contains("navbar-toggle")) return
+
     navbar.classList.add("toggle-active");
     btnToggleMenu.setAttribute("aria-expanded", "true");
-  } else if (e.target.classList.contains("navbar-container")) {
-    navbar.classList.remove("toggle-active");
-    btnToggleMenu.setAttribute("aria-expanded", "false");
   }
-});
+}
